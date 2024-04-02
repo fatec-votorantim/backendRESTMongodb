5c50edc561062b4753956b214b588d3ef0272375
Ricardo Leme <ricardo.leme@fatec.sp.gov.br>
2024-04-01 22:18:53 -0300

    Aula 01/04

--- api/routes/prestador.js
+++ api/routes/prestador.js
@@ -11,9 +11,12 @@ check('cnpj')
   .not().isEmpty().trim().withMessage('É obrigatório informar o cnpj')
   .isNumeric().withMessage('O CNPJ deve ter apenas números')
   .isLength({min:14, max:14}).withMessage('O CNPJ deve ter 14 números')
-  .custom(async (cnpj) => {
+  .custom(async (cnpj, { req }) => {
      const contaPrestador = await db.collection(nomeCollection)
-     .countDocuments({'cnpj': cnpj})
+     .countDocuments({
+      'cnpj': cnpj,
+      '_id': {$ne: new ObjectId(req.body._id)} //Exclui o docto atual
+    })
      if(contaPrestador > 0){
       throw new Error('O CNPJ informado já está cadastrado!')
      }
